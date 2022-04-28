1b0c1b1d238e40153d7e4c060e57adebee80b0ff
Sif4w <bmz.sifaw1@gmail.com>
2022-04-28 13:35:15 +0000

    blade :components,attributes,and tags ! + adding html them

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,26 +1,39 @@
 @extends('layout')
 
 @section('content')
-<h1>{{ $heading }} </h1>
+@include('partials._hero')
+@include('partials._search')
+<div
+class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
+>
+
+{{-- <h1>{{ $heading }} </h1> --}}
 
 @unless (count($listings) == 0)
 
 
 
 @foreach ($listings as $listing)
-    <h2>
+
+    {{-- <h2>
         <a href="/listings/{{$listing['id']}}">
         {{$listing['title']}} </a>
     </h2>
     <p>
         {{$listing['description']}}
 
-    </p>
+    </p> --}}
+
+      {{-- acces for (listing-card) component --}}
+      <x-listing-card :listing="$listing"/>
+    
+
 
 @endforeach
 @else
     <p>No listings Found !!!!!!!!!!</p>
 @endunless
+</div>
 
 @endsection
 
--- routes/web.php
+++ routes/web.php
@@ -26,12 +26,26 @@ Route::get('/' , function () {
 });
 
 // Single Listing
+    // Method 1
+            // Route::get('/listings/{id}', function($id) {
+            //     $listing = Listing::find($id);
+            //     if($listing) {
+            //         return view('listing', [
+            //             'listing' => $listing
+            //         ]);
+            //     }else {
+            //         abort('404');
+            //     }
+            // });
 
-Route::get('/listings/{id}', function($id) {
-    return view('listing', [
-        'listing' => Listing::find($id)
-    ]);
-});
+
+ // Method 2
+            Route::get('/listings/{listing}', function(Listing $listing) {
+                return view('listing', [
+                    'listing' => $listing
+                ]);
+            });
+
 
 
 
